8eebfc38c4d14dbeed8a5e7cc454039fc2420f5d
redjay48 <rajdey4892@gmail.com>
2022-12-08 14:18:44 +0530

    Add fibonacci recursion function

--- recursion-fibo.js
+++ recursion-fibo.js
@@ -7,3 +7,13 @@ function fiboLoop(n) {
   }
   console.log(array);
 }
+
+function fiboRec(n) {
+  if (n === 1) {
+    return [0];
+  } else if (n === 2) {
+    return [0, 1];
+  } else {
+    return [...fiboRec(n - 1), fiboRec(n - 1)[n - 2] + fiboRec(n - 1)[n - 3]];
+  }
+}
